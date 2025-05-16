6d7a445dcf34af609bb11837dc7b616ce31476cc
shrubfromtomorrow <shrubfromtomorrow@gmail.com>
2025-05-15 22:33:04 -0700

    version # update

--- BingoMode/Plugin.cs
+++ BingoMode/Plugin.cs
@@ -24,7 +24,7 @@ namespace BingoMode
     [BepInPlugin("nacu.bingomode", "Bingo", VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string VERSION = "1.04";
+        public const string VERSION = "1.05";
         public static bool AppliedAlreadyDontDoItAgainPlease;
         internal static ManualLogSource logger;
         private BingoModOptions _bingoConfig;
